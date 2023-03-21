df94d90e8d8e192298dcf18e436b29ed5260b4f7
Tester oprogramowania <martaglow@wp.pl>
2023-03-21 13:14:43 +0100

    Added update app v.1.0 & readme

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # Aplikacja dla osób dorosłych - zakup alkoholu w Pythonie
 
-## Funkcjonalności aplikacji w werski:
+## Funkcjonalności aplikacji w wersji:
 
 v.1.0
-- [x] obsługa osób dorosłych
-- [x] obsługa wymagań specjalnych dla osób +40
+O aplikacji
+Aplikacja po wpisaniu wieku, płci oraz wskazaniu regionu wybiera dla użytkownika odpowiednią ofertę lub nie zezwala na wejście do niej.
+
+Funkcjonalności aplikacji w wersji
+
+v.1.0
+
+ obsługa osób dorosłych
+ obsługa wymagań specjalnych dla osób +40
+
+v.2.0
+
+ obsługa kobiet 30+ 
+ obsługa regionu EUR/USA 
+ obsługa powyżej 120 lat 
+
+Status projektu
+Projekt jest w trakcie pracy.
+
+Możliwe ulepszenia
+Dodanie wyboru prefernecji smakowych, następnie wyświetlenie dopasowanych pod użytkownika produktów.
+Dodatkowe promocje dla poszczególnych grup wiekowych.
--- program.py
+++ program.py
@@ -1,14 +1,47 @@
 # v.1.0 dzialajacej aplikacji
 
-wiek = input("Podaj wiek użytkownika:")
-#sprawdzenie czy wiek jest liczbą całkowitą
-if wiek.isdigit() == False:
-    exit("Wiek musi byc liczba calkowita. Zamykam aplikację")
-wiek=int(wiek)
-if wiek>=18 and wiek<=40:
-    print("Witamy w apce. Mozesz kupować u nas alkohol")
-elif wiek>40:
-    print("Witamy w apce. Mozesz kupować u nas alkohol")
-    print("Prosze korzystaj z produktów z umiarem")
-else:
-    exit("Jestes za młody/a na alkohol. Zapraszamy na disney/com :)")
+print ("Witaj w naszym sklepie. Podaj potrzebne dane, aby moc korzystac ze sklepu.")
+
+region = input("Podaj region (EUR/USA)")
+plec = str(input("podaj swoja plec: "))
+
+region = str(region.upper())
+
+if region == "EUR":
+    wiek = input ("Podaj wiek uzytkownika: ")
+    if wiek.isdigit() == False:
+        exit("Wiek musi byc liczba calkowita. Zamykam aplikacje")
+
+    wiek=int(wiek)
+
+    if  wiek >= 18 and wiek <= 40:
+        print ("Witamy w apce. Mozesz kupic u nas alkohol")
+    elif wiek > 40 and wiek < 120:
+        print ("Witamy w apce. Mozesz kupic u nas alkohol")
+        print ("Prosze korzystaj z produktow z umiarem")
+    elif wiek > 120:
+        exit('Cos krecisz z tym wiekiem. Zamykam apke')
+    else:
+        exit("Jestes za mlody/a na alkohol. Zapraszamy na disney.com")
+elif region == "USA":
+    wiek = input ("Podaj wiek uzytkownika: ")
+    if wiek.isdigit() == False:
+        exit("Wiek musi byc liczba calkowita. Zamykam aplikacje")
+
+    wiek=int(wiek)
+
+    if  wiek >= 21 and wiek <= 40:
+        print ("Witamy w apce. Mozesz kupic u nas alkohol")
+    elif wiek >40 and wiek < 120:
+        print ("Witamy w apce. Mozesz kupic u nas alkohol")
+        print ("Prosze korzystaj z produktow z umiarem")
+    elif wiek > 120:
+        exit('Cos krecisz z tym wiekiem. Zamykam apke')
+    else:
+        exit("Jestes za mlody/a na alkohol. Zapraszamy na disney.com")
+else: 
+    exit ("Nie sprzedajemy w innych regionach")
+    
+if wiek > 30 and plec == "kobieta" or "K" or "F" or "k" or "f":
+    print(" +30 to aperol spritz pierwszy gratis!")
+    print("mozesz kupowac alkohol")
